ef11c188dd9da580ae5a0c44152230c46d576db1
carlitosalcala <carlos.herrero@live.com>
2018-06-15 10:02:42 +0200

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,8 +1,8 @@
-/* Programa 1 - Última prueba del acceso */
+/* Programa 1 - Ãšltima prueba del acceso */
 #include <stdio.h>
 int main()
 {
-printf("Bienvenido a la aplicación C para el ejercicio 1\nEsta ha sido creada en una Raspberry B++ usando VIM y el compilador GCC incluido.\n");
-printf("\nUn cordial saludo.\nCarlos Herrero");
+printf("Bienvenido a la aplicaciÃ³n C para el ejercicio 1\nEsta ha sido creada en una Raspberry B++ usando VIM y el compilador GCC incluido.\n");
+printf("\nUn cordial saludo.\n");
 return 0;
 }
